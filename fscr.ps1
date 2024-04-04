53438ea39742ef9df3c944ff57cf20176bbc785e
Direcka <libor.slepr@gmail.com>
2024-04-04 10:31:06 +0200

    Update fscr.ps1

--- fscr.ps1
+++ fscr.ps1
@@ -11,3 +11,5 @@ set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value "c:\t
 set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaperStyle -value 22
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 Stop-Process -Name "Explorer"
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+Stop-Process -Name "Explorer"
